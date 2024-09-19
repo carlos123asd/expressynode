ad2de42c5d29ea4ac3d056b4b5a4c5e6c3c95baa
carlos123asd <carlos-medin@hotmail.com>
2024-09-19 19:44:53 +0200

    Fix: doc

--- src/models/modelEmployee.ts
+++ src/models/modelEmployee.ts
@@ -1,20 +1,5 @@
 import {model} from 'mongoose'
 import Employee from '../interfaces/Employee';
 import { employeeSchema } from '../schema/schemaEmployee';
-import bcrypt from 'bcrypt'
-
-employeeSchema.pre('save', async function (next) {
-    const user = this;
-    if (!user.isModified('password') || !user.isNew) {
-      return next();
-    }
-    try {
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(user.password, salt);
-      next();
-    } catch (err) {
-      console.error(err)
-    }
-});
 
 export const Users = model<Employee>('User', employeeSchema);
